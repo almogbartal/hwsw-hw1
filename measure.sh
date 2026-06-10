7d274e3eb1bbcdb9c9263c9305fdf3938901acf9
George (Gosha) Ginzburg <ginzburggeorgy@gmail.com>
2026-06-10 22:29:28 +0300

    add perf stat script

---
+++ measure.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <program> [args...]"
+    exit 1
+fi
+
+
+perf stat -r 10 -e branch-instructions,branch-misses,cache-references,cache-misses,cpu-cycles,cpu-clock,page-faults ./firefly_sync_unoptimized 200
