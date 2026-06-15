ec1e08f2169d9e14b8fab1b8394c762be5dc2658
George (Gosha) Ginzburg <ginzburggeorgy@gmail.com>
2026-06-15 18:41:08 +0300

    measure script optins

--- measure.sh
+++ measure.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+repeat="-r 10"
+if [ "$1" = "--once" ]; then
+    repeat=""
+    shift
+fi
+
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 <program> [args...]"
+    echo "Usage: $0 [--once] <program> [args...]"
     exit 1
 fi
 
 # "$@" captures all arguments passed to the script
-perf stat -r 10 -e branch-instructions,branch-misses,cache-references,cache-misses,cpu-cycles,cpu-clock,page-faults "$@"
+perf stat $repeat -e branch-instructions,branch-misses,cache-references,cache-misses,cpu-cycles,cpu-clock,page-faults "$@"
